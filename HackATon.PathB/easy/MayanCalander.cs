5c0a45d393594e1e1537d04a93ed6a824bc5a393
Jarno Caenepeel <Jarno.caenepeel@student.howest.be>
2023-11-09 11:26:56 +0100

    Finish path B easy challenge

---
+++ HackATon.PathB/easy/MayanCalander.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using Newtonsoft.Json;
+
+namespace HackATon.PathB.easy
+{
+    public class MayanCalander
+    {
+        private HackTheFutureClient hackTheFutureClient;
+        public MayanCalander()
+        {
+            this.hackTheFutureClient = new HackTheFutureClient();
+            
+        }
+        public async Task<string> StartChallenge()
+        {
+            await hackTheFutureClient.Login(Constants.UserName, Constants.Password);
+            var result = await hackTheFutureClient.GetAsync("/api/path/b/easy/start");
+            if (!result.IsSuccessStatusCode)
+            {
+                return "Challenge not started";
+            }
+            return "Challenge started";
+        }
+        public async Task<MayanCalendarChallengeDto> GetChallengeInfo()
+        {
+            var result = await hackTheFutureClient.GetAsync("/api/path/b/easy/puzzle");
+            if (!result.IsSuccessStatusCode)
+            {
+                throw new Exception();
+            }
+            var content = await result.Content.ReadAsStringAsync();
+            var mayanCalanderDto = JsonConvert.DeserializeObject<MayanCalendarChallengeDto>(content);
+            return mayanCalanderDto;
+        }
+        public async Task<string> ClearChallenge()
+        {
+            try
+            {
+                var challengeInfo = await GetChallengeInfo();
+                var answer = GetAmountOfDay(challengeInfo);
+                await SendAnswerOfChallenge(answer);
+                return $"The answer is {answer}";
+            }
+            catch(Exception ex)
+            {
+                return "You did not solve the puzzle";
+            }
+            
+        }
+
+        private async Task SendAnswerOfChallenge(int answer)
+        {
+            var result = await hackTheFutureClient.PostAsJsonAsync<int>($"/api/path/b/easy/puzzle", answer);
+            if (!result.IsSuccessStatusCode)
+            {
+                throw new Exception();
+            }
+        }
+
+        private int GetAmountOfDay(MayanCalendarChallengeDto mayanChallenge)
+        {
+            var amountOfDays = 0;
+            if(mayanChallenge.StartDate.DayOfWeek.ToString() == mayanChallenge.Day)
+            {
+                amountOfDays++;
+            }
+            var date = mayanChallenge.StartDate;
+            do
+            {
+                date = date.AddDays(1);
+                if(date.DayOfWeek.ToString() == mayanChallenge.Day)
+                {
+                    amountOfDays++;
+                }
+
+            } while (date != mayanChallenge.EndDate);
+
+            return amountOfDays;
+        }
+
+    }
+}
