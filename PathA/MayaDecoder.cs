3f60ebad2f273d175e24c6f3bbc9739465fa7c5b
SiemenSlabbinck <Siemen.Slabbinck@student.ap.be>
2023-11-09 10:56:20 +0100

    Challenge1

---
+++ PathA/MayaDecoder.cs
@@ -0,0 +1,38 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathA
+{
+    public class MayaDecoder
+    {
+        public Dictionary<char, char> alphabet;
+
+        public MayaDecoder()
+        {
+            alphabet = HieroglyphAlphabet.Characters;
+        }
+
+    public string Decode(string input)
+        {
+            string decodedString = "";
+
+            foreach (char c in input)
+            {
+                try
+                {
+                    var value = alphabet[c];
+                    decodedString += value;
+                } catch (Exception e)
+                {
+                    decodedString += c;
+                }
+
+            }
+            return decodedString;
+        }
+    }
+}
---
+++ PathA/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Common;
+using PathA;
+using System.Net.Http.Json;
+using System.Security.Cryptography;
+using System.Text;
+
+HackTheFutureClient hackTheFutureClient = new HackTheFutureClient();
+await hackTheFutureClient.Login("*", "ETmpRH6seu");
+
+//A1
+Challenge1 challenge1 = new Challenge1();
+challenge1.Run();
+
+//A2
+
+
+Console.ReadLine();
